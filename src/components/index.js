7bfd4aa65c2a12d88dc6dbe5240e6c3b65bce828
Boy <daniel.onazi116@gmail.com>
2022-08-20 11:40:21 +0100

    Made a change

--- src/components/index.js
+++ src/components/index.js
@@ -4,4 +4,4 @@ export { default as Navbar } from './navbar/Navbar';
 export { default as Numbers } from './numbers/Numbers';
 export { default as FAQ } from './FAQ/FAQ';
 export { default as Checkbox } from './Checkbox/Checkbox';
-export { default as Copyright } from './Copyright';
+export { default as Copyright } from './copyright/Copyright';
